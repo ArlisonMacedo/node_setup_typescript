7c0cfc8a7d5e3468ed007bef9967bedc5e3bb8a5
ArlisonMacedo <arlisonmacedo526@gmail.com>
2020-06-19 21:41:23 -0300

    Add TS tsconfig-paths

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,15 @@
 import express from "express";
-
+import { UserControlller } from "@controllers/UserControllers";
 const app = express();
 
 app.get("/", (request, response) => {
   return response.json({
     message: "Hello World",
   });
+});
+
+app.get("/users", (request, response) => {
+  return response.send("My Eggs");
 });
 
 app.listen(3333);
